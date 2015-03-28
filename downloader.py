685b0aff7d7c0eeb88d8e2b6f56e06d3b72b9d22
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2015-03-28 18:26:35 -0400

    mockup for downloader

--- downloader.py
+++ downloader.py
@@ -1,6 +1,36 @@
 """
 Check the FEC website to find if a new filing has been posted.
-Grab the files and store them in the repository /data folder with new date.
+If there is a new filing store it in /data folder with new date.
 
 http://www.fec.gov/finance/disclosure/ftpdet.shtml
+
+indiv16.zip 
+oth16.zip
 """
+
+def get_file():
+    '''
+    Get file from URL
+    '''
+    pass
+
+def already_downloaded(file):
+    '''
+    Return true if we already have this version of the file
+    (check date and file hash). False otherwise
+    '''
+    pass
+
+def save(file):
+    '''
+    Assign proper namin and save file in the appropriate folder.
+    '''
+
+def download():
+    '''Grabs the latest.'''
+    my_file = get_file()
+
+    if already_downloaded(my_file):
+        print "Didn't save"
+    else:
+        save(my_file)
