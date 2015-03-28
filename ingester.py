019fa2cb4d2d71f3b7ccb8c7e193dd9e566d06dc
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2015-03-28 18:36:10 -0400

    shell for ingesting files into sqlite database

--- ingester.py
+++ ingester.py
@@ -1,3 +1,13 @@
 """
 Push new data to sqlite database.
 """
+
+def ingested(file):
+    '''Return true if file is already ingested, false otherwise'''
+    pass
+
+def ingest(file):
+    '''Ingest file into sqlite database'''
+    pass
+
+if __name__ == '__main__':
