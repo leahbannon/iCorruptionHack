552b974fde97e1839a6d4821b30a5621021e9fea
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2015-04-22 19:33:50 -0400

    add root path to app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 import json
 from peewee import *
 
 app = Flask(__name__)
+root = os.path.dirname(os.path.abspath(__file__))
 
 with open('keys.json','r') as keysfile:
     keys = json.load(keysfile)
